f5c5477efe69dce9275ee9f86cdb796413fa2edf
ori00raisfeld <raisfeldori@gmail.com>
2023-08-01 19:44:35 +0300

    working on the output

--- src/outputs.rs
+++ src/outputs.rs
@@ -3,54 +3,73 @@ outputs:
 the structs that store the results, so that they can be handled from the python side.
  */
 
-use std::env::var;
+use std::io::stderr;
+use std::ops::Deref;
+use pyo3::exceptions::{PyBaseException, PyException};
 use pyo3::prelude::*;
+use std::sync::mpsc;
 use crate::base_parser::*;
 use crate::errors::*;
+use tokio::task::{JoinHandle, spawn};
 
+#[pyclass]
+pub enum AllOutputs{
+    BaseOutput,
+}
 
 #[derive(Clone)]
 #[pyclass]
 pub struct BaseOutput {
     pub statements: Vec<BaseStatement>,
-    pub statement_errors: Vec<NotStatementError>,
     pub variables: Vec<BaseVar>,
-    pub variable_errors: Vec<NotVarError>,
-    pub unknown: Vec<String>,
+    pub executables: Vec<BaseExecutable>,
+    pub unknown: Vec<ShallowParsedLine>,
 }
 
+#[pymethods]
 impl BaseOutput{
-    pub fn from(shallow_code: Vec<ShallowParsedLine>) -> BaseOutput{
-        let mut variables: Vec<BaseVar> = Vec::new();
-        let mut variable_errors: Vec<NotVarError> = Vec::new();
-        let mut statements: Vec<BaseStatement> = Vec::new();
-        let mut statement_errors: Vec<NotStatementError> = Vec::new();
-        let mut unknown: Vec<String> = Vec::new();
-        for shallow_line in shallow_code {
+    #[staticmethod]
+    pub fn output_type() ->  AllOutputs {AllOutputs::BaseOutput}
+    pub fn statements(&self) -> Vec<BaseStatement> {self.statements.clone()}
+    pub fn variables(&self) -> Vec<BaseVar> {self.variables.clone()}
+    pub fn executables(&self) -> Vec<BaseExecutable> {self.executables.clone()}
+    pub fn unknown(&self) -> Vec<ShallowParsedLine> {self.unknown.clone()}
+}
+
+pub async fn create_base_output(shallow_code: Vec<ShallowParsedLine>) -> PyResult<BaseOutput>{
+    let mut threads: Vec<JoinHandle<()>> = Vec::new();
+    let (var_s, var_r) = mpsc::channel();
+    let (state_s, state_r) = mpsc::channel();
+    let (exe_s, exe_r) = mpsc::channel();
+    let (unknown_s, unknown_r) = mpsc::channel();
+
+    for shallow_line in shallow_code {
+        
+        threads.push(spawn(async move{
             match shallow_line.line_code_type {
                 CodeType::Variable => {
-                    let output = BaseVar::from(shallow_line);
-                    match output {
-                        Err(error) => variable_errors.push(error),
-                        Ok(var) => variables.push(var),
-                    }
+                    var_s.send(BaseVar::from(shallow_line));
                 }
                 CodeType::Statement => {
-                    let output = BaseStatement::from(shallow_line);
-                    match output {
-                        Err(error) => statement_errors.push(error),
-                        Ok(var) => statements.push(var),
-                    }
+                    state_s.send(BaseStatement::from(shallow_line));
                 }
-                _ => {unknown.push(shallow_line.actual_line)}
-            }
-        }
-        return BaseOutput {
-            variables: variables,
-            variable_errors: variable_errors,
-            statements: statements,
-            statement_errors: statement_errors,
-            unknown: unknown,
-        }
+                CodeType::Executable => {
+                    exe_s.send(BaseExecutable::from(shallow_line));
+                }
+                CodeType::Unknown => {unknown_s.send(shallow_line);}
+            };
+        }));
     }
+    let variables: Vec<BaseVar> = Vec::new();
+    let statements: Vec<BaseStatement> = Vec::new();
+    let executables: Vec<BaseExecutable> = Vec::new();
+    let unknown: Vec<ShallowParsedLine> = Vec::new();
+
+    return Ok(BaseOutput {
+        variables: variables,
+        statements: statements,
+        executables: executables,
+        unknown: unknown,
+        }
+    )
 }
