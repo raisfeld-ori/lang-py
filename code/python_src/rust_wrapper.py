cfa8a98f9568f9e9c8a7445fac4109da0501d2a1
ori00raisfeld <raisfeldori@gmail.com>
2023-08-03 23:36:47 +0300

    working on a wrapper for the output

--- code/python_src/rust_wrapper.py
+++ code/python_src/rust_wrapper.py
@@ -1,9 +1,9 @@
 from compiler import compiler
-
+from python_src.cli import Console
 class output:
     def __init__(self, rust_output):
         self.raw_output = rust_output
 
         match rust_output.output_type():
             case compiler.classes.AllOutputs.BaseOutput:
-                ...
+                console.log("is basic")
